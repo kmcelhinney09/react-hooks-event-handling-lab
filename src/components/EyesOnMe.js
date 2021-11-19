da24b22251083ce0f8af686af6ea77eb8e5acd42
mcelhinneyk <mr.mcelhinney@gmail.com>
2021-11-18 20:35:09 -0800

    commit tree faf66bfdadb1c9f813879f6a5711803a8c59f1d2

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function handelonBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+    function handelonFocus(){
+        console.log('Good!')
+    }
+    return (
+        <button onFocus={handelonFocus} onBlur={handelonBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
